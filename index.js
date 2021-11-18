e8d0112f26ced1dd1fd9962ddb0411682ad9ad0c
knguyen5259 <91755924+knguyen5259@users.noreply.github.com>
2021-11-17 19:58:13 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var modal = document.getElementById("lilliput-pop-up");
-var btn = document.getElementById("lilliput-button");
-var span = document.getElementById("lilliput-close");
+const modal = document.getElementById("lilliput-pop-up");
+const btn = document.getElementById("lilliput-button");
+const span = document.getElementById("lilliput-close");
 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -16,9 +16,9 @@ window.onclick = function(event) {
   }
 }
 
-var modal2 = document.getElementById("brobdingnag-pop-up");
-var btn2 = document.getElementById("brobdingnag-button");
-var span2 = document.getElementById("brobdingnag-close");
+const modal2 = document.getElementById("brobdingnag-pop-up");
+const btn2 = document.getElementById("brobdingnag-button");
+const span2 = document.getElementById("brobdingnag-close");
 
 btn2.onclick = function() {
   modal2.style.display = "block";
@@ -34,9 +34,9 @@ window.onclick = function(event) {
   }
 }
 
-var modal3 = document.getElementById("laputa-pop-up");
-var btn3 = document.getElementById("laputa-button");
-var span3 = document.getElementById("laputa-close");
+const modal3 = document.getElementById("laputa-pop-up");
+const btn3 = document.getElementById("laputa-button");
+const span3 = document.getElementById("laputa-close");
 
 btn3.onclick = function() {
   modal3.style.display = "block";
@@ -52,9 +52,9 @@ window.onclick = function(event) {
   }
 }
 
-var modal4 = document.getElementById("glub-pop-up");
-var btn4 = document.getElementById("glub-button");
-var span4 = document.getElementById("glub-close");
+const modal4 = document.getElementById("glub-pop-up");
+const btn4 = document.getElementById("glub-button");
+const span4 = document.getElementById("glub-close");
 
 btn4.onclick = function() {
   modal4.style.display = "block";
@@ -67,5 +67,59 @@ span4.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal4.style.display = "none";
+  }
+}
+
+const modal5 = document.getElementById("lug-pop-up");
+const btn5 = document.getElementById("lug-button");
+const span5 = document.getElementById("lug-close");
+
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal5.style.display = "none";
+  }
+}
+
+const modal6 = document.getElementById("england-pop-up");
+const btn6 = document.getElementById("england-button");
+const span6 = document.getElementById("england-close");
+
+btn6.onclick = function() {
+  modal6.style.display = "block";
+}
+
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal6.style.display = "none";
+  }
+}
+
+const modal7 = document.getElementById("hou-pop-up");
+const btn7 = document.getElementById("hou-button");
+const span7 = document.getElementById("hou-close");
+
+btn7.onclick = function() {
+  modal7.style.display = "block";
+}
+
+span7.onclick = function() {
+  modal7.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal7.style.display = "none";
   }
 }
